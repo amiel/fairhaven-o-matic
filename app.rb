2903bf74c0a5c26bfc8dec7ab6f649602096fe47
Amiel Martin <amiel.martin@gmail.com>
2010-05-25 17:35:51 -0700

    a prototype done

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
-  require 'rubygems'
-  require 'sinatra'
-  get '/' do
-    'Hello world!'
-  end
+require 'rubygems'
+require 'sinatra'
+
+require 'markov_chainer'
+
+get '/' do
+  mc = MarkovChainer.new(2)
+  mc.add_text(File.read('seeds.txt'))
+  mc.generate_sentence
+end
 
